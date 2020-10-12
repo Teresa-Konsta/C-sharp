9ec59fe1197ee604fb86be88c3b00c85e0c55655
Tereza Konstari <71859225+Teresa-Konsta@users.noreply.github.com>
2020-10-11 22:55:16 -0400

    Add files via upload

---
+++ w5 json/Json/Json/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteJsonFile();
+        }
+
+        public static void WriteJsonFile()
+        {
+            Employee myEmployee = new Employee();
+
+            Console.Write("Enter Employee Number: ");
+            myEmployee.EmployeeNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Employee Name: ");
+            myEmployee.Name = Console.ReadLine();
+
+            Console.Write("Enter Employee Salary: ");
+            myEmployee.Salary = double.Parse(Console.ReadLine());
+
+            FileStream outFile = new FileStream("Employee.json", FileMode.Create, FileAccess.Write);
+            StreamWriter streamWriter = new StreamWriter(outFile);
+
+            string json = JsonConvert.SerializeObject(myEmployee); // serialize employee
+
+            streamWriter.WriteLine(json);
+            streamWriter.Close();
+            outFile.Close();
+        }
+
+        public static void ReadTextFile()
+        {
+            FileStream inFile = new FileStream("EmployeeInfo.json",
+                FileMode.Open,
+                FileAccess.Read);
+
+            StreamReader streamReader = new StreamReader(inFile);
+            string line = streamReader.ReadLine();
+            Employee myEmployee = JsonConvert.DeserializeObject<Employee>(line);
+
+            Console.WriteLine($"Employee number: {myEmployee.EmployeeNumber}");
+            Console.WriteLine($"Employee name: {myEmployee.Name}");
+            Console.WriteLine($"Employee salary: {myEmployee.Salary:C}");
+
+            streamReader.Close();
+            inFile.Close();
+        }
+
+
+    }
+}
---
+++ w5 json/jsonfile/JsonFile/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonFile
+{
+    class Employee
+    {
+        private int employeeNumber;
+        private string name;
+        private double salary;
+
+        public int EmployeeNumber
+        {
+            get
+            {
+                return employeeNumber;
+            }
+            set
+            {
+                employeeNumber = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public double Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+    }
+}
