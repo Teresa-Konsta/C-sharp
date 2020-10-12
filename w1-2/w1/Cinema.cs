dc92ce0404c61446b86d154191e661e6bd79b05e
Tereza Konstari <71859225+Teresa-Konsta@users.noreply.github.com>
2020-10-11 22:50:41 -0400

    Add files via upload

---
+++ w1-2/w1/Cinema.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w1Q1
+{
+    class Cinema
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name of the movie: ");
+            string movieName = Console.ReadLine();
+            int duration = 90;
+            string output = DisplayFilm(movieName, ref duration);
+            Console.WriteLine(output);
+            Console.WriteLine(DisplayFilm(movieName, ref duration));
+        }
+
+        static string DisplayFilm(string movieName, ref int duration)
+        {
+            if (duration != 90)
+            {
+                Random randomDuration = new Random();
+                duration = randomDuration.Next(60, 120);
+            }
+            //Console.WriteLine(movieName);
+            //Console.Write($"- {duration} minutes");
+            string output = movieName + " - " + duration + "minutes";
+            return output;
+        }
+    }
+}
---
+++ w1-2/w1Q2/TaxMath.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w1Q2
+{
+    class TaxMath
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the price: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the tax rate: ");
+            string taxRateOriginal = Console.ReadLine();
+            int taxRateInt;
+            double taxRateDb;
+
+            if (int.TryParse(taxRateOriginal, out taxRateInt))
+            {
+                double priceFinal = CalculateTax(price, taxRateInt);
+                Console.WriteLine($"The price including tax is: {priceFinal:C}");
+            }
+            else if(double.TryParse(taxRateOriginal, out taxRateDb))
+            {
+                double priceFinal = CalculateTax(price, taxRateDb);
+                Console.WriteLine($"The price including tax is: {priceFinal:C}");
+            }
+
+            double price1 = CalculateTax(79.95, 0.13);
+            double price2 = CalculateTax(79.95, 13);
+            Console.WriteLine($"Price 1: {price1:C}");
+            Console.WriteLine($"Price 2: {price2:C}");
+        }
+        static double CalculateTax(double price, double taxRate)
+        {
+            double priceWithTax = price + (price * taxRate);
+            return priceWithTax;
+        }
+
+        static double CalculateTax(double price, int taxRate)
+        {
+            double priceWithTax = price + (price * (taxRate/100));
+            return priceWithTax;
+        }
+    }
+}
